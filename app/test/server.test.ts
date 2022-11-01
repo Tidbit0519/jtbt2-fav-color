5d5547af7884dc1aca32a1c5b5e8230fd7629f35
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-11-01 12:21:12 -1000

    Mock Route Implementation again

--- app/test/server.test.ts
+++ app/test/server.test.ts
@@ -22,3 +22,38 @@ describe('GET /foo', () => {
     expect(response.body.bar).toEqual('hello world')
   })
 })
+
+describe('POST /addFavoriteColor', () => {
+  test('should return 200', async () => {
+    const response = await request(app).get('/addFavoriteColor')
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('GET /listFavoriteColors', () => {
+  test('should return 200', async () => {
+    const response = await request(app).get('/listFavoriteColors')
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('GET /getFavoriteColors', () => {
+  test('should return 200', async () => {
+    const response = await request(app).get('/getFavoriteColor')
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('PUT /updateFavoriteColor', () => {
+  test('should return 200', async () => {
+    const response = await request(app).get('/updateFavoriteColor')
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('DELETE /removeFavoriteColor', () => {
+  test('should return 200', async () => {
+    const response = await request(app).get('/removeFavoriteColor')
+    expect(response.statusCode).toBe(200)
+  })
+})
