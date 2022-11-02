ef45d68761fe3b096768f8fd5ea3e3c6c00d98ff
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-11-01 14:21:54 -1000

    Update app-dev.tf

--- iac/dev/app/app-dev.tf
+++ iac/dev/app/app-dev.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.0.11"
+  required_version = "1.3.2"
   backend "s3" {
     bucket         = "terraform-state-storage-863362256468"
     dynamodb_table = "terraform-state-lock-863362256468"
