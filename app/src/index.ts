16859915a3c2ba183d2f511ec3966f154eefd783
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-11-08 09:12:10 -1000

    Connect to Dynamo in AWS

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,13 @@
 import server from './server'
 import defaultLogger from '@byu-oit/logger'
 import { Logger } from 'pino'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION ?? 'us-west-2',
+  endpoint: process.env.DYNAMODB_ENDPOINT // set the DYNAMODB_ENDPOINT to http://localhost:8000 when using local dynamo
+})
 
 export const logger: Logger = defaultLogger()
 
@@ -11,6 +18,9 @@ async function run (): Promise<void> {
   }).on('error', err => {
     logger.error({ err }, 'Error starting the app')
   })
+  const ddbDocClient = DynamoDBDocumentClient.from(client)
+  const response = await ddbDocClient.send(new ScanCommand({ TableName: 'jtbt2-fav-color-dev' }))
+  logger.info({ response }, 'DynamoDB response')
 }
 
 run()
