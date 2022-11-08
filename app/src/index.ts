5a124ffe8b461586b522141a92981df6f1f07e6d
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-11-08 10:23:22 -1000

    Connect to Dynamo in AWS

--- app/src/index.ts
+++ app/src/index.ts
@@ -14,7 +14,7 @@ export const logger: Logger = defaultLogger()
 async function run (): Promise<void> {
   const app = await server()
   app.listen(8080, () => {
-    logger.info('listening on port 8080')
+    logger.info('listening on port 8080!')
   }).on('error', err => {
     logger.error({ err }, 'Error starting the app')
   })
